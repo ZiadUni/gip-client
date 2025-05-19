9694f46b8e2ff7232fafad30d2087d70ce534e79
ZiadUni <ziad.hussein@gu.edu.eg>
2025-05-19 06:44:21 +0300

    Add placeholder in feedbackmanagementtab.jsx so vercel stops crying.

---
+++ src/pages/ManagerDashboard/FeedbackManagementTab.jsx
@@ -0,0 +1,9 @@
+const FeedbackManagementTab = () => {
+  return (
+    <div className="text-center mt-4">
+      <h5>Feedback tab coming soon...</h5>
+    </div>
+  );
+};
+
+export default FeedbackManagementTab;
