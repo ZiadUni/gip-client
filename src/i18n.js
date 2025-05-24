172cc1e15a1dc81381b3543c1d3aea02467e6f6e
ZiadUni <ziad.hussein@gu.edu.eg>
2025-05-24 14:14:31 +0300

    Add some translations for home page and fix underlying import bug.

--- src/i18n.js
+++ src/i18n.js
@@ -5,20 +5,16 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import translationEN from './locales/en/translation.json';
 import translationAR from './locales/ar/translation.json';
 
-const resources = {
-  en: { translation: translationEN },
-  ar: { translation: translationAR }
-};
-
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
+    resources: {
+      en: { translation: translationEN },
+      ar: { translation: translationAR }
+    },
     fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false
-    }
+    interpolation: { escapeValue: false }
   });
 
 export default i18n;
